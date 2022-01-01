43d33699bda6fc741a1d674d88fd3a0e4cf084b2
EmilyC <eclairebenn@outlook.com>
2021-12-31 16:34:01 -0800

    Add Project Seed Data

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 import db from "./models";
+import { projects } from "./seeders/projects";
 import { users } from "./seeders/users";
 
 const createUsers = () => {
@@ -10,8 +11,13 @@ const createUsers = () => {
     db.User.create(user);
   });
 };
+const createProjects = () => {
+  projects.map((project) => {
+    db.Project.create(project);
+  });
+};
 
-createUsers();
+createProjects();
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
---
+++ seeders/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    title: "Laser Ruler",
+    active: true,
+  },
+  {
+    title: "Electric Cat Food Feeder",
+    active: true,
+  },
+  {
+    title: "Mobile Plant Doctor",
+    active: true,
+  },
+];
