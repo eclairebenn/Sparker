9c1d95dde1d0f6b722766beb8d693e8cd02ab0b7
EmilyC <eclairebenn@outlook.com>
2022-01-04 22:05:28 -0800

    Project CRUD operations
    -getById, getAll, create, update, delete functions for project
    controller and routes.
    
    Authored-by: Emily Bennett

--- index.ts
+++ index.ts
@@ -1,34 +1,31 @@
 import express from "express";
 import cors from "cors";
 import * as bodyParser from "body-parser";
-
 import projectRoutes from "./server/routes/project.routes";
+import db from "./server/models";
+import { projectbackings } from "./server/seeders/projectbackings";
+import { projects } from "./server/seeders/projects";
+import { users } from "./server/seeders/users";
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-import db from "./server/models";
-// import { projectbackings } from "./seeders/projectbackings";
-// import { projects } from "./seeders/projects";
-// import { users } from "./seeders/users";
-
 //Seed Functions
-// const createUsers = () => {
-//   users.map((user) => {
-//     db.User.create(user);
-//   });
-// };
-// const createProjects = () => {
-//   projects.map((project) => {
-//     db.Project.create(project);
-//   });
-// };
-// const createProjectBackings = () => {
-//   projectbackings.map((projectbacking) => {
-//     db.ProjectBacking.create(projectbacking);
-//   });
-// };
+const createUsers = () => {
+  users.map((user) => {
+    db.User.create(user);
+  });
+};
+const createProjects = () => {
+  projects.map((project) => {
+    db.Project.create(project);
+  });
+};
+const createProjectBackings = () => {
+  projectbackings.map((projectbacking) => {
+    db.ProjectBacking.create(projectbacking);
+  });
+};
 
-// createProjectBackings();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
--- server/routes/project.routes.ts
+++ server/routes/project.routes.ts
@@ -4,6 +4,11 @@ import * as projectController from "../controllers/project/project.controller";
 
 const projectRouter = Router();
 
-projectRouter.get("/", projectController.findAll);
+projectRouter
+  .get("/", projectController.findAll)
+  .get("/:id", projectController.findById)
+  .post("/", projectController.create)
+  .put("/:id", projectController.update)
+  .delete("/:id", projectController.deleteById);
 
 export default projectRouter;
